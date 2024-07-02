f0322526e92766b854f25ca41a8725094759bfb9
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-03 02:50:38 +0530

    Code Fixes

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[84vh] px-4">
           <Image 
             source={images.logo}
             className="w-[130px] h-[84px]"
