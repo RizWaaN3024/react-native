542904744a8cf3b23df8ff681f8354c8e2ef5b7a
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-03 01:25:08 +0530

    Added Nativewind

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,14 +3,11 @@ import React from 'react'
 import { Slot, Stack } from 'expo-router'
 
 const RootLayout = () => {
-  return (
-    <>
-        <Text>Header</Text>
-        <Slot />
-        <Text>Footer</Text>
-    </>
-
-  )
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+    )
 }
 
 export default RootLayout
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile Page</Link>
     </View>
   );
 }
