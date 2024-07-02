76cf486d7f36b58be456d4e6da9901dfb1f1e6b0
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-03 01:52:01 +0530

    Configured File and Folder Structure

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-red-300">
-      <Text className="text-3xl">Hello</Text>
+      <Text className="text-3xl font-pextrabold">Hello</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>Go to Profile Page</Link>
     </View>
