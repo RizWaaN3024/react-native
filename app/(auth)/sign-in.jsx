b0a0baa8a4dc650f323030faf8a819a0bd3162c7
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-06 23:41:45 +0530

    Added sign in and sign out functions

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants"
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
+import { signIn } from '../../lib/appwrite'
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -12,8 +13,26 @@ const SignIn = () => {
     password:''
   })
   const [issubmitting, setIsSubmitting] = useState(false)
-  const submit = () => {
+  const submit = async () => {
 
+    if(!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await signIn(form.email, form.password)
+
+      // set it to global state
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+    createUser();
   }
   return (
     <SafeAreaView className="bg-primary h-full">
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,10 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants"
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { createUser } from '../../lib/appwrite'
 
 const SignUp = () => {
@@ -14,7 +14,25 @@ const SignUp = () => {
     password:''
   })
   const [issubmitting, setIsSubmitting] = useState(false)
-  const submit = () => {
+  const submit = async () => {
+
+    if(!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username)
+
+      // set it to global state
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
     createUser();
   }
   return (
@@ -48,7 +66,7 @@ const SignUp = () => {
             otherStyles="mt-7"
           />
           <CustomButton 
-            title="Sign In"
+            title="Sign Up"
             handlePress={submit}
             containerStyles={`mt-7`}
             isLoading={issubmitting}
